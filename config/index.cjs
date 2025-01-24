bda4b7afa06698f74882189a4a0c844d9182e87a
Ying Yujie <2282879197@qq.com>
2025-01-24 10:06:04 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小缘宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oeUhM7NEROmSE6g9x0opP0bYsEsA',
+      id: 'oeUhM7HyguXQwPrEUk0RUeNkCeBY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'lgPHgaTlfTrYc__pW0qzv8vDNmYzDsf-WaFHjwB8s8A',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
